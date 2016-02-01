8fc3158da117bff80865444f57624eb41d50ed63
Michael Gagliardo <mgagliardo91@gmail.com>
2016-02-01 15:07:44 +0000

    Updates to app viewports and bulk email process.

--- scripts/SendBulkEmail.js
+++ scripts/SendBulkEmail.js
@@ -7,6 +7,7 @@ var configDB = require('../config/config.js');
 var localConfig = require('../config/localConfig');
 var fs = require('fs');
 var async = require('async');
+var i = 1;
 
 if (process.argv.length != 4) {
     console.log('Invalid arguments');
@@ -26,26 +27,31 @@ mongoose.connection.on('connected', function() {
             return console.log(err);
         }
         
-        async.each(users, function(user, cb) {
-            var template = Handlebars.compile(html);
-            var contentHtml = Handlebars.compile(content);
-            var contentMessage = contentHtml({serverURL: localConfig.serverURL});
-            var message = template({user: user, htmlContent: contentMessage, serverURL: localConfig.serverURL});
-            
-            Mailer.sendMail(user.local.email, process.argv[3], message, function(err, result) {
-               if (err) {
-                    return cb(err);
-                }
-                    
-                return cb();
-            });
-        }, function(err) {
-            if (err) {
-                console.log(err);
-            }
-            
-            console.log('Email broadcasted to all users successfully.');
+        console.log('Broadcasting email to [' + users.length + '] users.');
+        async.eachSeries(users, function(user, cb) {
+            setTimeout(function() {
+                sendMail(user, html, content, cb);
+            }, 1000);
+        }, function() {
+            console.log('Email broadcasting completed.');
             mongoose.disconnect();
         });
     });
 });
+
+function sendMail(user, html, content, cb) {
+    var template = Handlebars.compile(html);
+    var contentHtml = Handlebars.compile(content);
+    var contentMessage = contentHtml({serverURL: localConfig.serverURL});
+    var message = template({user: user, htmlContent: contentMessage, serverURL: localConfig.serverURL});
+    
+    Mailer.sendMail(user.local.email, process.argv[3], message, function(err, result) {
+       if (err) {
+           console.log('[' + i++ + ']: Error sending email to [' + user.local.email + ']: ' + err);
+            return cb();
+        }
+            
+        console.log('[' + i++ + ']: Email sent to: ' + user.local.email);
+        return cb();
+    });
+}
