2af09d7e139029d4f7dcc0c8705086103f436557
Michael Gagliardo <mgagliardo@gmail.com>
2017-04-10 08:48:01 -0400

    Updated package.json to remove old/unused dependencies and updated adminApi route name

--- app/routes/adminApi.js
+++ app/routes/adminApi.js
@@ -1,159 +1,82 @@
 var Error = require('../utils/error');
+var Access = require('../utils/access');
 var _ = require('underscore');
 var async = require('async');
 
-var User = require('../models/user');
-
-var AdminController = require('../controllers/admin');
-var DiscController = require('../controllers/disc');
-var UserController = require('../controllers/user');
-var EventController = require('../controllers/event');
-var FeedbackController = require('../controllers/feedback');
-
-var socketManager = require('../objects/socketCache.js');
-
-var Access = require('../utils/access.js');
+var DiscController = require('../controllers/disc')
+var UserController = require('../controllers/user')
 
 var Passport;
 
-// app/oauthRoutes.js
+// app/adminRoutes.js
 module.exports = function(app, passport) {
-
     Passport = passport;
 
-    app.route('/user')
-        .get(Access.hasAccess, function(req, res) {
-            UserController.getAllUsers(parsePagination(req.query), function(err, pager) {
-                if (err)
-                    return res.json(err);
+    app.use(Access.adminAccess);
 
-                return res.json(pager);
+    app.route('/discs/top')
+        .get(function(req, res, next) {
+            DiscController.getTopDiscs(req.query, function(err, discs) {
+                if (err)
+                    return next(err);
+
+                return res.json(discs);
             });
         });
 
-    app.route('/active')
-        .get(hasAccess, function(req, res) {
-            var users = [];
-            async.each(socketManager.getSocketSessions(), function(socketSession, cb) {
-                UserController.getUser(socketSession.userId, function(err, user) {
-                    if (err)
-                        return cb(err);
+    app.route('/discs/recent')
+        .get(function(req, res, next) {
+            DiscController.getLastCreated(req.query, function(err, discs) {
+                if (err)
+                    return next(err);
 
-                    users.push(user);
-                    cb();
-                });
-            }, function(err, results) {
-                return res.json(users);
+                return res.json(discs);
             });
         });
 
-     app.route('/user/geo')
-        .get(hasAccess, function(req, res) {
-            if (!req.query.loc || !req.query.radius)
-                return res.json(Error.createError('Invalid location or radius.', Error.invalidDataError));
-
-            UserController.getUsersByArea(req.query.loc, req.query.radius, function(err, users) {
+    app.route('/users/top')
+        .get(function(req, res, next) {
+            UserController.getTopUsers(req.query, function(err, users) {
                 if (err)
-                    return res.json(err);
+                    return next(err);
 
                 return res.json(users);
             });
         });
 
-    app.route('/disc')
-        .get(hasAccess, function(req, res) {
-            DiscController.getAllDiscs(parsePagination(req.query), function(err, pager) {
+    app.route('/users/online')
+        .get(function(req, res, next) {
+            UserController.getOnlineUsers(req.query, function(err, users) {
                 if (err)
-                    return res.json(err);
+                    return next(err);
 
-                return res.json(pager);
-            });
+                return res.json(users);
+            })
         });
 
-    app.route('/event')
-        .get(hasAccess, function(req, res) {
-            EventController.getAllEvents(parsePagination(req.query), function(err, pager) {
+    app.route('/users/recent/join')
+        .get(function(req, res, next) {
+            UserController.getRecentlyJoined(req.query, function(err, users) {
                 if (err)
-                    return res.json(err);
+                    return next(err);
 
-                return res.json(pager);
-            });
+                return res.json(users);
+            })
         });
 
-    app.route('/feedback')
-        .get(hasAccess, function(req, res) {
-            FeedbackController.getAllFeedback(parsePagination(req.query), function(err, pager) {
+    app.route('/users/recent/online')
+        .get(function(req, res, next) {
+            UserController.getRecentlyActive(req.query, function(err, users) {
                 if (err)
-                    return res.json(err);
+                    return next(err);
 
-                return res.json(pager);
-            });
+                return res.json(users);
+            })
         });
 
-    app.get('*', function(req, res){
-       res.json(401, Error.createError('Unknown path', Error.unauthorizedError));
-    });
-}
 
-function parsePagination(query) {
-    var parsed;
-    var i = 0;
-    var sort = [], filter = {};
 
-    var size = (parsed = parseInt(query.size)) && parsed > 0 ? parsed : 50;
-    var page = (parsed = parseInt(query.page)) && parsed > 0 ? parsed : 1;
-
-    if (query.sort) {
-        for (i = 0; i < query.sort.length; i++) {
-            var sorter = query.sort[i].split(',');
-            sort.push([sorter[0], parseInt(sorter[1])]);
-        }
-    }
-
-    if (query.filter && _.isArray(query.filter)) {
-        for (i = 0; i < query.filter.length; i++) {
-            var filterOpt = query.filter[i].split(',');
-            filter[filterOpt[0]] = filterOpt[1];
-        }
-    }
-
-    return {size: size, page: page, sort: sort, filter: filter};
-}
-
-function hasAccess(req, res, next) {
-
-    if (req.isAuthenticated()) {
-
-        if (req.admin) {
-            return next();
-        } else {
-            AdminController.validateAdmin(req.user._id, function(err, admin) {
-                if (err) {
-                    return res.json(401, err);
-                }
-
-                req.admin = admin;
-                return next();
-            });
-        }
-    } else {
-        Passport.authenticate('bearer', { session : false }, function(err, user, info) {
-            if (err) { return next(err); }
-
-            if (!user) {
-                return res.json(401, Error.createError('Access to this page requires an account.', Error.unauthorizedError));
-            }
-
-            req.user = user;
-
-            AdminController.validateAdmin(req.user._id, function(err, admin) {
-                if (err) {
-                    return res.json(401, err);
-                }
-
-                req.admin = admin;
-                return next();
-            });
-        })(req, res, next);
-    }
+    app.get('*', function(req, res, next) {
+		next(Error.createError('Unknown path', Error.unauthorizedError));
+	});
 }
