11a23d77fb3eff0f11eeb6c0b792cefd4ea2d83d
Michael Gagliardo <mgagliardo@gmail.com>
2017-05-02 08:50:32 -0400

    Cleaning up unused code to prepare for mobile site development

--- public/src/js/internal/internal.js
+++
@@ -1,9 +0,0 @@
-var adminUrl = "/admin";
-var url = adminUrl + "/api";
-
-$(document).ready(function() {
-
-	$('li.sidebar-select.nav').click(function() {
-		window.location = $(this).attr('href');
-	});
-});
